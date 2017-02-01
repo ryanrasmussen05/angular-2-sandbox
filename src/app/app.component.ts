f73206c6503da2f923a6445b43e104dc96211da7
ryan.rasmussen <ryan.rasmussen@issinc.com>
2017-02-01 11:11:51 -0600

    move app component html to own file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,23 +5,7 @@ import '../../semantic/dist/semantic.min.css';
 
 @Component({
     selector: 'rr-my-app',
-    template: `
-    <h1>{{title}}</h1>
-    <nav>
-        <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
-        <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
-    </nav>
-    <router-outlet></router-outlet>
-    <div class="ui dropdown">
-  <input type="hidden" name="gender">
-  <i class="dropdown icon"></i>
-  <div class="default text">Gender</div>
-  <div class="menu">
-    <div class="item" data-value="male">Male</div>
-    <div class="item" data-value="female">Female</div>
-  </div>
-</div>
-  `,
+    templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
