4a422fe8350e58bd1f5aec68d58692e25cd5b109
Ryan Rasmussen <ryan.rasmussen@aviture.us.com>
2017-06-25 19:41:12 -0500

    fix monty hall page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,9 +18,7 @@ import { CarSimComponent } from "./sandbox/physics-js/car-sim/car-sim-component"
 import { FireworksComponent } from './sandbox/canvas/fireworks/fireworks.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomepageComponent } from './homepage/homepage.component';
-import { InfoBoxComponent } from './info-box/info-box.component';
 import { MontyHallComponent } from "./sandbox/monty-hall/monty-hall.component";
-import { NavigationMenuComponent } from './navigation-menu/navigation-menu.component';
 import { ParticlesComponent } from './sandbox/canvas/particles/particles.component';
 import { ParticlesTwoComponent } from "./sandbox/physics-js/particles-two/particles-two.component";
 import { PhysicsIntroComponent } from './sandbox/physics-js/physics-intro/physics-intro.component';
@@ -51,9 +49,7 @@ import { StaticContentService } from './static-content/static-content.service';
         FireworksComponent,
         FooterComponent,
         HomepageComponent,
-        InfoBoxComponent,
         MontyHallComponent,
-        NavigationMenuComponent,
         ParticlesComponent,
         ParticlesTwoComponent,
         PhysicsIntroComponent,
--- src/app/navigation-menu/navigation-menu.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'rr-navigation-menu',
-    templateUrl: './navigation-menu.component.html'
-})
-export class NavigationMenuComponent {
-}
