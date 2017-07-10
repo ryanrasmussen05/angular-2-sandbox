bf57e65fe70c5f9f85a534b78124c8e5e515bee1
Ryan Rasmussen <ryan.rasmussen@aviture.us.com>
2017-07-10 18:40:58 -0500

    add rectangle button

--- src/app/sandbox/d3/event.planner.component.ts
+++ src/app/sandbox/d3/event.planner.component.ts
@@ -13,8 +13,8 @@ export class EventPlannerComponent implements AfterViewInit {
     @HostBinding('class') hostClass = 'fullscreen-graphics layout-column';
     @ViewChild('d3') svgElement: ElementRef;
 
-    circles: EventPlanner.CircleData[];
-    rectangles: EventPlanner.RectangleData[];
+    circles: EventPlanner.CircleData[] = [];
+    rectangles: EventPlanner.RectangleData[] = [];
     svg: Selection<SVGElement, {}, HTMLElement, any>;
 
     width: number;
@@ -27,42 +27,13 @@ export class EventPlannerComponent implements AfterViewInit {
         this.width = this.svgElement.nativeElement.offsetWidth;
         this.height = this.svgElement.nativeElement.offsetHeight;
 
-        this.circles = range(10).map(() => {
-            return {
-                x: Math.round(Math.random() * (this.width - this.radius * 2) + this.radius),
-                y: Math.round(Math.random() * (this.height - this.radius * 2) + this.radius)
-            };
+        range(10).map(() => {
+            this.addCircle();
         });
 
-        this.rectangles = range(10).map(() => {
-            return {
-                x: Math.round(Math.random() * (this.width - this.radius * 2)),
-                y: Math.round(Math.random() * (this.height - this.radius * 2)),
-                height: Math.round(Math.random() * 50),
-                width: Math.round(Math.random() * 50)
-            }
+        range(10).map(() => {
+            this.addRectangle();
         });
-
-        this.svg.selectAll('circle')
-            .data(this.circles)
-            .enter().append('circle')
-            .attr('cx', (data: EventPlanner.CircleData) => data.x)
-            .attr('cy', (data: EventPlanner.CircleData) => data.y)
-            .attr('r', this.radius)
-            .style('fill', 'black')
-            .call(drag<SVGCircleElement, EventPlanner.CircleData>()
-                .on('drag', EventPlanner.dragCircleHandler));
-
-        this.svg.selectAll('rect')
-            .data(this.rectangles)
-            .enter().append('rect')
-            .attr('x', (data: EventPlanner.RectangleData) => data.x)
-            .attr('y', (data: EventPlanner.RectangleData) => data.y)
-            .attr('width', (data: EventPlanner.RectangleData) => data.width)
-            .attr('height', (data: EventPlanner.RectangleData) => data.height)
-            .style('fill', 'teal')
-            .call(drag<SVGRectElement, EventPlanner.RectangleData>()
-                .on('drag', EventPlanner.dragRectangleHandler));
     }
 
     addCircle() {
@@ -80,5 +51,25 @@ export class EventPlannerComponent implements AfterViewInit {
             .style('fill', 'orange')
             .call(drag<SVGCircleElement, EventPlanner.CircleData>()
                 .on('drag', EventPlanner.dragCircleHandler));
+    }
+
+    addRectangle() {
+        this.rectangles.push({
+            x: Math.round(Math.random() * (this.width - this.radius * 2)),
+            y: Math.round(Math.random() * (this.height - this.radius * 2)),
+            height: Math.round(Math.random() * 50),
+            width: Math.round(Math.random() * 50)
+        });
+
+        this.svg.selectAll('rect')
+            .data(this.rectangles)
+            .enter().append('rect')
+            .attr('x', (data: EventPlanner.RectangleData) => data.x)
+            .attr('y', (data: EventPlanner.RectangleData) => data.y)
+            .attr('width', (data: EventPlanner.RectangleData) => data.width)
+            .attr('height', (data: EventPlanner.RectangleData) => data.height)
+            .style('fill', 'purple')
+            .call(drag<SVGRectElement, EventPlanner.RectangleData>()
+                .on('drag', EventPlanner.dragRectangleHandler));
     }
 }
