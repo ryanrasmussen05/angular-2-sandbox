835cac9525b6429887e84329dc6fddf6017f6402
Ryan Rasmussen <ryan.rasmussen@aviture.us.com>
2017-10-19 09:58:20 -0500

    replace all layout classes with basscss

--- src/app/sandbox/canvas/particles/particles.component.ts
+++ src/app/sandbox/canvas/particles/particles.component.ts
@@ -14,7 +14,7 @@ interface PhysicsNormal {
 })
 
 export class ParticlesComponent implements AfterViewInit, OnDestroy {
-    @HostBinding('class') hostClass = 'fullscreen-graphics layout-column';
+    @HostBinding('class') hostClass = 'fullscreen-graphics flex-column';
     @ViewChild('canvas') canvasElement: ElementRef;
     @ViewChild('canvasWrapper') canvasWrapperElement: ElementRef;
 
